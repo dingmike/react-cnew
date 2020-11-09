4492062c0f92f098a6d1d1dde6c0231eac72ef7e
zd <mikezhangding@hotmail.com>
2020-11-09 11:48:11 +0800

    init2sdf

--- src/pages/index/home/index.js
+++ src/pages/index/home/index.js
@@ -13,7 +13,12 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-           arr: [1,2,3,4]
+           arr: [1,2,3,4],
+            firstImg:{
+               src: 'https://img-blog.csdnimg.cn/20190227134034236.png?x-oss-process=image/watermark,type_ZmFuZ3poZW5naGVpdGk,shadow_10,text_aHR0cHM6Ly9ibG9nLmNzZG4ubmV0L2xpaHVpaHVpMjAxNg==,size_16,color_FFFFFF,t_70',
+                width: 320,
+                height: 640,
+            }
         };
     }
     componentDidMount() {
@@ -24,8 +29,8 @@ class Home extends Component {
         const {arr} = this.state;
         let {courseList, loading} = this.props;
         return (
-                <div>
-                    <FormattedMessage
+                <div className={styles['pageContainer']}>
+                   {/* <FormattedMessage
                         id="HELLO_WORLD"
                     />
                     <FormattedDate
@@ -46,7 +51,18 @@ class Home extends Component {
                         return (
                             <div key={item}>{item}</div>
                         )
-                    })}
+                    })}*/}
+                    <WingBlank size="md">
+                        <div className={styles['header-title']}>
+                            <h2 className={styles}>Today Reading</h2>
+                            <p>Read more, learn more.</p>
+                        </div>
+                        <div className={styles.pageContentMiddle}>
+                            <div className={styles.contentImg}>
+                                <img src={this.state.firstImg.src} alt=""/>
+                            </div>
+                        </div>
+                    </WingBlank>
                 </div>
         );
     }
